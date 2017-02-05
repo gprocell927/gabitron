9dc2002bcfee26c651b1a1384ee86e81eabbeff2
Gabi Procell <gprocell927@gmail.com>
2017-02-04 17:12:52 -0700

    wip: creating nav bar component

---
+++ src/components/Nav/Nav.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+   <ul class="nav-bar">
+     <li><a href="#home">Home</a></li>
+     <li><a href="#about">About</a></li>
+     <li><a href="#projects">Projects</a></li>
+     <li><a href="#contact">Contact</a></li>
+   </ul>
+  )
+}
+
+
+export default Nav
